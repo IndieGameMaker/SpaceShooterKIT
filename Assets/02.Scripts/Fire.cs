6d1b64877c91d4d0d539661fa5135bf405463678
Lee JaeHyun <myleje@gmail.com>
2022-12-17 11:43:03 +0900

    .

--- Assets/02.Scripts/Fire.cs
+++ Assets/02.Scripts/Fire.cs
@@ -7,15 +7,15 @@ public class Fire : MonoBehaviour
     [SerializeField] private Transform firePos;
     [SerializeField] private GameObject bulletPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 총알 생성
+            // Instantiate (생성할객체, 위치, 각도)
+
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
 
     }
 }
