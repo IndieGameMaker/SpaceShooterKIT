b64226e43532b2375e2361e906b68905964a599a
Lee JaeHyun <myleje@gmail.com>
2022-12-17 11:11:29 +0900

    .

---
+++ Assets/02.Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+`using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+
+    void Start()
+    {
+        // Rigidbody 컴포넌트 추출후 변수에 할당
+        rb = GetComponent<Rigidbody>();
+
+        // 힘(Force)을 가하는 함수
+        rb.AddRelativeForce(Vector3.forward * 1000.0f); // 1000 뉴튼(N)
+    }
+}
